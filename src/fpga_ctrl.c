3799a47684e215674a0f762d4bed127459e087ce
johenleem <kettyleem@163.com>
2025-06-02 12:09:07 +0800

    add the bram access race
    Signed-off-by: johenleem <kettyleem@163.com>

--- src/fpga_ctrl.c
+++ src/fpga_ctrl.c
@@ -12,12 +12,14 @@
 #include <linux/types.h>
 #include <linux/spi/spidev.h>
 #include <sys/mman.h>
+#include <pthread.h>
 #include "fpga_ctrl.h"
 
 #define BRAM_BASE_ADDR (0x80003000)
 #define BRAM_SIZE (0x1000)
 struct fpga_bram_handle {
 	int fd;
+	pthread_spinlock_t spinlock;
 	u32 *base_addr;
 };
 
@@ -42,13 +44,19 @@ void *fpga_res_init(void)
 	char *devname = "/dev/mem";
 
 	if ((fpga->fd = open(devname, O_RDWR)) < 0) {
-		printf("open %s failed\n", devname);
+		printf("%s open %s failed\n", __func__, devname);
 		return NULL;
 	}
 
 	fpga->base_addr = mmap(NULL, BRAM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fpga->fd, BRAM_BASE_ADDR);
 	if (fpga->base_addr == MAP_FAILED) {
-		printf("map fpga base addr failed!\n");
+		printf("%s map fpga base addr failed!\n", __func__);
+		return NULL;
+	}
+
+	/* just access with multithread environment */
+	if (pthread_spin_init(&fpga->spinlock, PTHREAD_PROCESS_PRIVATE) != 0) {
+		printf("%s spinlock init failed!\n", __func__);
 		return NULL;
 	}
 
@@ -61,7 +69,9 @@ int fpga_bram_write(void *handle, u32 offset, u32 val)
 		return -1;
 
 	struct fpga_bram_handle *fpga = handle;
+	pthread_spin_lock(&fpga->spinlock);
 	fpga->base_addr[offset] = val;
+	pthread_spin_unlock(&fpga->spinlock);
 
 	return 0;
 }
@@ -73,4 +83,15 @@ u32 fpga_bram_read(void *handle, u32 offset)
 
 	struct fpga_bram_handle *fpga = handle;
 	return fpga->base_addr[offset];
+}
+
+void fpga_res_close(void *handle)
+{
+	if (handle == NULL) {
+		printf("%s handle invalid\n", __func__);
+		return;
+	}
+
+	struct fpga_bram_handle *fpga = handle;
+	pthread_spin_destroy(&fpga->spinlock);
 }
