60dc7d2700aa2fe005c2ca852d47303c9205a1f8
johenleem <kettyleem@163.com>
2025-08-14 22:33:19 +0800

    adjust version autogen mechism
    Signed-off-by: johenleem <kettyleem@163.com>

--- include/version.h
+++ include/version.h
@@ -1,9 +1,7 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-#define MAINCTRL_VERSION "2025-08-11,20:44:32"
-#define GIT_VERSION "4a3c768"
-#define RELEASE_VERSION "V1.0.0.0"
+#include "auto_ver.h"
 
 void version_show(void);
 
